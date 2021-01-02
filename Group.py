f6321cecb448edd4939a66363fe4ece98e7d0cce
yl12053 <qq18929261343@gmail.com>
2021-01-02 23:59:15 +0800

    添加Weather plugin
    需要图片原(将会被添加)

--- Group.py
+++ Group.py
@@ -41,9 +41,35 @@ def Block(Type,GroupID,MsgSeq,MsgRandom,QQ):
         POST.GroupMsg(msg=f'监测到违规信息,已经撤回,类型为{Type}',groupid=GroupID,picurl=0,picbase=0)
         POST.SetShutUpUser(qq=QQ,time=config['TextShutupTime'],groupid=GroupID)
     
-
+def Weather(msg, QQ, GroupID):
+    if msg.split()[0] == "查询天气" and len(msg.split()) == 2:
+        city = zhconv.convert(msg.split()[1], "zh-hans").replace("'","").lower().replace(' ','')
+        if '#' in city or '--' in city or '"' in city:
+            POST.GroupMsg(msg = "请不要尝试进行SQL注入。\n怀疑违规行为已经向所有风纪委员通报。", groupid = GroupID, picurl = 0, picbase = 0, atUser = QQ)
+            POST.GroupMsg(msg = "QQ号为"+str(QQ)+"的用户怀疑正在进行SQL注入，请注意且自行判断其违规行为并予以惩罚。\n消息内容: \n"+msg, groupid = 835021978, picurl = 0, picbase = 0, atUser = 0)
+        else:
+            sqlr = sql.read('SELECT * FROM city WHERE cityZh = '+city+' or cityEn = '+city)
+            if len(sqlr) == 0:
+                sqlr2 = sql.read('SELECT * FROM city WHERE provinceZh = '+city+' or provinceEn = '+city)
+                if len(sqlr2) > 0:
+                    POST.GroupMsg(msg = "请输入具体城市名作为参数。属于"+city[0][4]+"省的城市有: \n"+"\n".join([x[2] for x in read('SELECT * FROM city WHERE provinceZh="'+city+'" or provinceEn="'+city+'"')])+"\n。", groupid = GroupID, picurl = 0, picbase = 0, atUser = QQ)
+                else:
+                    POST.GroupMsg(msg = "无查询结果。\n请确认是否有错别字或者拼写错误。", groupid = GroupID, picurl = 0, picbase = 0, atUser = QQ)
+            else:
+                raw = json.loads(urllib.request.urlopen("https://tianqiapi.com/api?version=v6&appid=18224395&appsecret=lgCc5VqI&cityid="+str(sqlr[0][0])).read())
+                rtst = sqlr[0][2]+"的天气状况: \n"
+                rtst+= "温度: "+str(raw['tem'])+"℃ ("+str(raw['tem2'])+"℃-"+str(raw['tem1'])+"℃)\n"
+                rtst+= "湿度："+raw['humidity']+'\n'
+                rtst+= raw['wea']+'\n'
+                rtst+= "吹"+raw['win']+' '+raw['win_speed']+'[PICFLAG]'
+                try:
+                    picf = open('./plugin/weather/'+raw['wea_img']+'.png', 'rb').read()
+                    pbase = base64.b64encode(picf).decode()
+                except:
+                    pbase = 0
+                POST.GroupMsg(msg = rtst, groupid = GroupID, picurl = 0, picbase = pbase, atUser = QQ)
 
 def Group(msg,QQ,GroupID):
     ShutUp(msg,QQ,GroupID)
     LoginBilibili(msg,QQ,GroupID)
-
+    Weather(msg, QQ, GroupID)
